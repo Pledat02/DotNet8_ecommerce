22d3053641c13f28fc54e1316ee230e6502a9cc9
=Dat <phamledatctbp@gmail.com>
2024-08-05 13:34:55 +0700

    update build

--- Ecommerce/obj/Debug/net8.0/Ecommerce.AssemblyInfo.cs
+++ Ecommerce/obj/Debug/net8.0/Ecommerce.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e7011e49cbc676cd73cf4f73c5b50ac8d4ce623")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c19812823b43971767faf91aefb7dffaf89955a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
