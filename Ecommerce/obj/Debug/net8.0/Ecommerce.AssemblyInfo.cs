ffd7a19854ff0db72081f23542e67f4fc1c538db
Tam <21130168@st.hcmuaf.edu.vn>
2024-08-05 08:49:08 +0700

    update user, voucher, voucher_user service and controller

--- Ecommerce/obj/Debug/net8.0/Ecommerce.AssemblyInfo.cs
+++ Ecommerce/obj/Debug/net8.0/Ecommerce.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4f74bda6ea2d44368cb8c565de6d926a8f09ad1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e578a4387549918cd03fa0f5d0aa165e08126cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
